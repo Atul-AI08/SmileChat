1c487cccf5e6588db763588d6857783fce7f42cc
Atul Kumar <atul_2101ai08@iitp.ac.in>
2023-10-16 15:10:52 +0530

    basic setup

---
+++ client/src/utils/FirebaseConfig.js
@@ -0,0 +1,15 @@
+import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBODBspxsmSisaUAzvJRoEmt0SZOHr8eKc",
+  authDomain: "clone-10747.firebaseapp.com",
+  projectId: "clone-10747",
+  storageBucket: "clone-10747.appspot.com",
+  messagingSenderId: "310914693517",
+  appId: "1:310914693517:web:bde7feeae3a48d2cc4e5c4",
+  measurementId: "G-L5YBT0G9CH"
+};
+
+const app = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(app);
